43fb2188e7e1f29fd0f2f382806f94181fb7f7ff
ozanarmagan <oza-armagan@yandex.com>
2021-02-19 04:40:10 +0300

    son entryler eklendi

--- controllers/user.js
+++ controllers/user.js
@@ -1,35 +1,54 @@
-const axios = require('axios');
 const cheerio = require('cheerio');
-
-
+const entryJs = require('./entry');
+const puppeteer = require('puppeteer');
 
 const EKSI_URL = "https://eksisozluk.com";
 const USER_URL = EKSI_URL + "/biri/";
 
 exports.getUser = async function(nick) {
-  let response;
   try {
-    response = await axios.get(USER_URL + nick);
-  } catch (err) { 
-    return { error: err.message };
-  }  
-  let user = {};
-  let $ = cheerio.load(response.data, { decodeEntities: false });
-  
-  let user_nick = $("#user-profile-title").attr("data-nick");
-  let entry_count_total = $("#entry-count-total").text().trim();
-  let entry_count_lastmonth = $("#entry-count-lastmonth").text().trim();
-  let entry_count_lastweek = $("#entry-count-lastweek").text().trim();
-  let entry_count_today = $("#entry-count-today").text().trim();
-  let last_entry_time = $("#last-entry-time").text().trim();
-  
-  user = {
-    nick: user_nick,
-    entry_count_total,
-    entry_count_lastmonth,
-    entry_count_lastweek,
-    entry_count_today,
-    last_entry_time,
+    const browser = await puppeteer.launch();
+    const [page] = await browser.pages();
+
+    await page.goto(USER_URL+nick, { waitUntil: 'networkidle0' });
+    const data = await page.evaluate(() => document.querySelector('*').outerHTML);
+
+    let user = {};
+    let $ = cheerio.load(data, { decodeEntities: false });
+    
+    let user_nick = $("#user-profile-title").attr("data-nick");
+    let entry_count_total = $("#entry-count-total").text().trim();
+    let entry_count_lastmonth = $("#entry-count-lastmonth").text().trim();
+    let entry_count_lastweek = $("#entry-count-lastweek").text().trim();
+    let entry_count_today = $("#entry-count-today").text().trim();
+    let last_entry_time = $("#last-entry-time").text().trim();
+    let last_entries_ids = [];
+    let last_entries = [];
+
+    $("#topic").find(".topic-item").each((index,element) => {
+      $(element).find("ul > li").each((index2,element2) => {
+        ($(element2).attr("data-id")) != undefined ? last_entries_ids.push($(element2).attr("data-id")) : (null) 
+      });
+    });
+    for(id of last_entries_ids)
+    {
+      var entry = await entryJs.getEntry(id);
+      last_entries.push(entry);
+    }
+
+    user = {
+      nick: user_nick,
+      entry_count_total,
+      entry_count_lastmonth,
+      entry_count_lastweek,
+      entry_count_today,
+      last_entry_time,
+      last_entries,
+    }
+    return user;
   }
-  return user;
+  catch(err)
+  {
+    console.log(err)
+  }
 }
