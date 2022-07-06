37024c56984e929a6e3aa0d301b2e17f3210ded2
Onur Alp Akin <onralpakin@gmail.com>
2022-07-06 12:18:26 +0300

    fix user.js

--- controllers/user.js
+++ controllers/user.js
@@ -3,27 +3,44 @@ const cheerio = require('cheerio');
 const URLS = require('./constants');
 
 
-exports.getUser = async function(nick) {
-  let response;
+exports.getUser = async function (nick) {
+  let response, last_ten_entry;
   try {
     response = await axios.get(URLS.USER + "/" + nick);
-  } catch (err) { 
+
+    // https://eksisozluk.com/son-entryleri?nick=ssg&p=1
+    last_ten_entry = await axios.get(`${URLS.LAST_ENTRIES}?nick=${nick}&p=1`,
+      {
+        headers:
+        {
+          'X-Requested-With': 'XMLHttpRequest'
+        }
+      }
+    );
+  } catch (err) {
     return { error: err.message };
-  }  
+  }
+
   let user = {};
   let $ = cheerio.load(response.data, { decodeEntities: false });
-  
-  let quote_entry_title = $("#quote-entry > h2 > a").text();
-  let quote_entry_body = $("#quote-entry > div > p").html().trim();
-  let quote_entry_date = $("#quote-entry > footer > a").text();
-  let quote_entry_url = $("#quote-entry > footer > a").attr("href");
+
   let user_nick = $("#user-profile-title").attr("data-nick");
   let entry_count_total = $("#entry-count-total").text().trim();
-  let entry_count_lastmonth = $("#entry-count-lastmonth").text().trim();
-  let entry_count_lastweek = $("#entry-count-lastweek").text().trim();
-  let entry_count_today = $("#entry-count-today").text().trim();
-  let last_entry_time = $("#last-entry-time").text().trim();
-  
+
+  $ = cheerio.load(last_ten_entry.data, { decodeEntities: false });
+  const quote_entry_string=".pinned-icon-container + .topic-item"
+
+  let quote_entry_title = $(`${quote_entry_string} > h1 > a`).text();
+  let quote_entry_body = ""
+  let quote_entry_date = ""
+  let quote_entry_url = ""
+
+  if (quote_entry_title) {
+    quote_entry_body = $(`${quote_entry_string} > ul > li > div`).html().trim();
+    quote_entry_date = $(`${quote_entry_string} > ul > li > footer > div.info > div.entry-footer-bottom > div.footer-info > div:eq(1) > a`).html();
+    quote_entry_url = $(`${quote_entry_string} > ul > li > footer > div.info > div.entry-footer-bottom > div.footer-info > div:eq(1) > a`).attr("href");
+  } 
+
   user = {
     nick: user_nick,
     quote_entry_title,
@@ -31,10 +48,7 @@ exports.getUser = async function(nick) {
     quote_entry_date,
     quote_entry_url,
     entry_count_total,
-    entry_count_lastmonth,
-    entry_count_lastweek,
-    entry_count_today,
-    last_entry_time,
   }
+  
   return user;
 }
